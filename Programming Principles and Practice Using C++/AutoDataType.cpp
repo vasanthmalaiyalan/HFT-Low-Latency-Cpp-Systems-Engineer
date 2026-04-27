127969e28087adbf58107e917ea1ba036572ad07
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-27 05:01:01 +0000

    AutoDataType.cpp,a.out,

---
+++ Programming Principles and Practice Using C++/AutoDataType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    auto x = 10;      // int
+    auto y = 3.14;  // double
+    auto c = 'A'; // char
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << c << '\n';
+
+    return 0;
+}
