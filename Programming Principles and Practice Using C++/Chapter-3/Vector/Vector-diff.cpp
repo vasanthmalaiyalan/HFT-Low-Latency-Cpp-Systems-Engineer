f38565b938bb541b3196a29abda0d5f7f2bb74b7
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-27 18:15:34 +0000

    Vector-diff.cpp,a.out,

---
+++ Programming Principles and Practice Using C++/Chapter-3/Vector/Vector-diff.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    std::vector<int> nums = {1,2,3};
+    std::vector<std::string> names = {"Ram", "Kumar"};
+
+    std::cout << nums[0] << '\n';
+    std::cout << names[1] << '\n';
+}
