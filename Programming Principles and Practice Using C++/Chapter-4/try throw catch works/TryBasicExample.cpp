4513267be4e5686bd2e760d0211f2b09cdb27ce4
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-29 08:26:26 +0000

    TryBasicExample.cpp,a.out,

--- Programming Principles and Practice Using C++/Chapter-4/try throw catch works/TryBasicExample.cpp
+++ Programming Principles and Practice Using C++/Chapter-4/try throw catch works/TryBasicExample.cpp
@@ -5,7 +5,7 @@ int main() {
 
     try {
         std::cout << "Start\n";
-        throw std::runtime_error("Error!");
+        throw std::runtime_error("Error!\n");
         std::cout << "End!\n"; // if throw success next End block didn't run 
     }
     catch (std::runtime_error& e) {
