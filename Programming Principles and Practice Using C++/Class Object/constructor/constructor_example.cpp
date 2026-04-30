fbc79e2cab38a08c2b10c605fd921f87c58f6940
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-30 19:25:59 +0000

    a.out,constructor_example.cpp,

---
+++ Programming Principles and Practice Using C++/Class Object/constructor/constructor_example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// Constructor example
+class BankAccount {
+    private:
+      double balance;
+
+    public:
+      // Constructor (class name same, no return type)
+      BankAccount(double initial) {
+        std::cout << "Constructor called Automatically\n";
+        balance = initial;
+      }  
+
+      void showBalance() {
+        std::cout << "Balance: " << balance << "\n";
+      }
+};
+
+int main() {
+    // object create
+    BankAccount acc(1000);
+    // Constructor automatically call 
+
+    acc.showBalance();
+
+    return 0;
+}
