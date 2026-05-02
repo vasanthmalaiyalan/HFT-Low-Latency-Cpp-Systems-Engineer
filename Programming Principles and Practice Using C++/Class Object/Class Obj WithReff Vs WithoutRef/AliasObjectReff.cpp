070c69cef9da43100e19dc388ce6aa681a0b8657
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-02 09:33:43 +0000

    AliasObjectReff.cpp,a.out,

---
+++ Programming Principles and Practice Using C++/Class Object/Class Obj WithReff Vs WithoutRef/AliasObjectReff.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Box {
+    public:
+      int value;
+};
+
+void change_with(Box& x) { // assign inside main  starting address  store in &x
+    std::cout << "Function address (&x): " << &x << "\n";
+    x.value = 300; // memory + inside metalayout value offset
+}
+
+int main() {
+    Box b;
+    b.value = 10;
+
+    std::cout << "Main address (&b): " << &b << "\n";
+    change_with(b);
+}
