27179ffa55bef0579cf0c171bcf43d575d569a06
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-06 05:32:47 +0000

    First_Version.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-5/Calculator/app/First Version/First_Version.cpp
@@ -0,0 +1,37 @@
+// calculator_first_version_fixed.cpp
+// Trying the first version
+// Recursive descent parser with Token_stream
+
+#include <iostream>
+#include <stdexcept>
+#include <cctype>
+
+// =============================================
+// Token 
+// =============================================
+
+class Token {
+    public:
+      char kind; // what kind of token
+      double value; // for numbers
+      
+      Token (char k)
+          : kind{k}, value{0,0}
+      {
+
+      } 
+      
+      Token (char k, double v)
+         : kind{k}, value{v}
+       {
+
+       }  
+};
+
+// ==============================================
+// Error helper
+// ==============================================
+
+void error (const std::string& msg) {
+    throw std::runtime_error(msg);
+}
